58c49e05485a2f3da4189a0546f1252f97316d27
Ryan West <ryanwest6@gmail.com>
2019-03-18 23:04:55 -0600

    turn into function that returns books in strings
    Signed-off-by: Ryan West <ryanwest6@gmail.com>

--- downloader.py
+++ downloader.py
@@ -2,30 +2,42 @@
 
 import requests
 
-# modify these to change range  
-startNum = 1000
-filesToRetrieve = 5
+# Modify these to change book range. Keep printToFile false to have the texts returned, each as a tuple of
+# (book number, book text in one long string).
+def downloadBookRange(startNum=1000, filesToRetrieve=5, printToFile=False):
 
-curFile = startNum
+    curFile = startNum
 
-while curFile < startNum + filesToRetrieve:
+    # list of all books so far
+    books = []
 
-    filename = str(curFile) + '.txt'
-    filepath = 'books/' + filename
-    url = 'http://www.gutenberg.org/files/' + str(curFile) + '/' + filename
-    url2 = 'http://www.gutenberg.org/files/' + str(curFile) + '/' + str(curFile) + '-0.txt'
+    while curFile < startNum + filesToRetrieve:
 
-    r = requests.get(url)
-    if r.status_code == 200:
-        with open(filepath, 'w') as f:
-            f.write(r.text)
-        print('Book #' + str(curFile) + ' retrieved.')
-    else:
-        r = requests.get(url2)
+        filename = str(curFile) + '.txt'
+        filepath = 'books/' + filename
+        url = 'http://www.gutenberg.org/files/' + str(curFile) + '/' + filename
+        url2 = 'http://www.gutenberg.org/files/' + str(curFile) + '/' + str(curFile) + '-0.txt'
+
+        r = requests.get(url)
         if r.status_code == 200:
-            with open(filepath, 'w') as f:
-                f.write(r.text)
-            print('Book #' + str(curFile) + ' retrieved (using ' + str(curFile) + '-0.txt).')
+            if printToFile:
+                with open(filepath, 'w') as f:
+                    f.write(r.text)
+            else:
+                books.append((curFile, r.text))
+            print('Book #' + str(curFile) + ' retrieved.')
         else:
-            print('Book #' + str(curFile) + ' failed: ' + str(r.status_code))
-    curFile += 1
+            r = requests.get(url2)
+            if r.status_code == 200:
+                if printToFile:
+                    with open(filepath, 'w') as f:
+                        f.write(r.text)
+                else:
+                    books.append((curFile, r.text))
+                print('Book #' + str(curFile) + ' retrieved (using ' + str(curFile) + '-0.txt).')
+            else:
+                print('Book #' + str(curFile) + ' failed: ' + str(r.status_code))
+        curFile += 1
+
+    if not printToFile:
+        return books
